38a18a73972c1a83ebe0423a864c9823ace2a578
Brauuwu <survivalminecraft080@gmail.com>
2026-09-06 13:48:54 +0700

    Initial commit cho Lab1-02_Serial_Communication

---
+++ README.md
@@ -0,0 +1,5 @@
+# Lab1-02_Serial_Communication
+
+**Giao tiếp Serial PC**
+
+Bài thực hành thuộc Lab1 (OBC) trong hệ thống CubeSat-LMS.
---
+++ src/main.cpp
@@ -0,0 +1,3 @@
+#include <Arduino.h>
+void setup() { Serial.begin(115200); }
+void loop() { Serial.println("Hello HEPTA-SAT / CubeSat!"); delay(1000); }
