0888b445fb93a621e1b8277a482b936100ac308f
Brauuwu <survivalminecraft080@gmail.com>
2026-09-06 18:16:44 +0700

    init

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,8 @@
 #include <Arduino.h>
-void setup() { Serial.begin(115200); }
-void loop() { Serial.println("Hello HEPTA-SAT / CubeSat!"); delay(1000); }
+void setup() {
+    Serial.begin(115200);
+}
+void loop() {
+    Serial.println("Hello HEPTA-SAT / CubeSat!");
+    delay(1000);
+}
